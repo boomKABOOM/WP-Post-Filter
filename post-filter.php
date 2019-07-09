345cd0bc0b52d5a64b0b771d072b922c4d1fa95b
bmKBM <beesbane@mac.com>
2019-07-09 11:11:32 -0400

    init project

---
+++ post-filter.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: bmKBM Post Filter
+ * Plugin URI: http://www.boomkaboom.com
+ * Description: An elegant solution to filter and display posts simply
+ * Version: 1.0
+ * Author: boomKABOOM Studio
+ * Author URI: http://www.boomkaboom.com
+ */
+ ?>
